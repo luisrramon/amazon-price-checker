a7dc308032835a5ae6f5d7363d2f05f5f62ae5f1
Luis Ramon <luis.ramon4799@gmail.com>
2025-03-09 00:53:32 -0500

    Added comments in database.py for better understanding in logic

--- database.py
+++ database.py
@@ -2,15 +2,24 @@ from sqlalchemy import create_engine, Column, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Database URL for SQLite
 DATABASE_URL = "sqlite:///./products.db"
+
+# Create a new SQLite database engine instance
 engine = create_engine(DATABASE_URL)
+
+# Create a configured "Session" class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for our classes definitions
 Base = declarative_base()
 
+# Define the Product class
 class Product(Base):
     __tablename__ = "products"
-    url = Column(String, primary_key=True, index=True)
-    title = Column(String)
-    price = Column(Float)
+    url = Column(String, primary_key=True, index=True) # URL as a primary key
+    title = Column(String) # Product title
+    price = Column(Float) # Product price
 
+# Create all tables in the database
 Base.metadata.create_all(bind=engine)
